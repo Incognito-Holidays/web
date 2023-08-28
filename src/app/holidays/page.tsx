00cc7df5f670b35443da89f9bf380d91bad57872
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2023-08-28 12:32:32 +0530

    refactor: replace starting prince with learn more button in packages card component

--- src/app/holidays/page.tsx
+++ src/app/holidays/page.tsx
@@ -8,7 +8,7 @@ const HolidaysPage: React.FC = () => {
         Packages for your next vacation
       </h1>
       <div className='mt-8 grid place-items-center gap-4 gap-y-7 md:grid-cols-3 md:gap-y-10'>
-        <PackagesCard title='Bali' startingPrice={10000} img={g1} slug='bali' />
+        <PackagesCard title='Bali' img={g1} slug='bali' />
       </div>
     </>
   );
--- src/components/packages-card.tsx
+++ src/components/packages-card.tsx
@@ -1,21 +1,14 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { LiaRupeeSignSolid } from 'react-icons/lia';
 import type { StaticImageData } from 'next/image';
 
 type CardProps = {
   title: string;
-  startingPrice: number;
   img: StaticImageData;
   slug: string;
 };
 
-const PackagesCard: React.FC<CardProps> = ({
-  title,
-  startingPrice,
-  img,
-  slug
-}) => {
+const PackagesCard: React.FC<CardProps> = ({ title, img, slug }) => {
   return (
     <Link href={`holidays/${slug}`}>
       <div className='rounded-xl border-2 p-3 transition-all duration-200 hover:shadow-lg'>
@@ -28,13 +21,9 @@ const PackagesCard: React.FC<CardProps> = ({
         />
         <div className='mt-3 flex items-center justify-between text-gray-800'>
           <p className='text-xl font-medium'>{title}</p>
-          <div>
-            <p className='flex items-center text-xl font-medium text-cyan-700'>
-              <LiaRupeeSignSolid className='h-5 w-5' />
-              <span>{startingPrice.toFixed(2)}</span>
-            </p>
-            <p className='text-right text-xs text-gray-400'>Starting price</p>
-          </div>
+          <button className='rounded-lg bg-cyan-600 p-2 text-sm text-white transition duration-200 hover:opacity-80'>
+            Learn more
+          </button>
         </div>
       </div>
     </Link>
