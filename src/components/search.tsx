c56d71c46c33d76787e58a0ba0205f256fd4bcd8
lucy2512 <mail4parthib@gmail.com>
2023-09-02 20:53:45 +0530

    feat: made serach box functional

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,20 +1,28 @@
+'use client';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
 const Search: React.FC = () => {
+  const router = useRouter();
+  const [holidayDest, setHolidayDest] = useState('');
+
   return (
     <div className='ml-11 mr-11 flex items-center  space-x-5 py-6'>
-      <select className='w-full appearance-none rounded-md border bg-white p-4 text-blue-950 shadow-sm outline-none focus:border-blue-950'>
-        <option>Choose Package</option>
-        <option>Srilanka Package</option>
-        <option>Bali Package</option>
-        <option>Thailand Package</option>
-        <option>Vietnam Package</option>
+      <select
+        className='w-full appearance-none rounded-md border bg-white p-4 text-blue-950 shadow-sm outline-none focus:border-blue-950'
+        onChange={(e) => setHolidayDest(e.target.value)}
+      >
+        <option value=''>Choose Package</option>
+        <option value='srilanka'>Srilanka Package</option>
+        <option value='bali'>Bali Package</option>
+        <option value='thailand'>Thailand Package</option>
+        <option value='vietnam'>Vietnam Package</option>
       </select>
 
       <button
-        className='rounded border
-         border-blue-950 bg-cyan-500 px-4 py-2 
-         font-bold text-white 
-          transition duration-200 hover:bg-blue-950 
-         '
+        className='borderborder-blue-950 rounded bg-cyan-500 px-4 py-2 font-bold text-white 
+          transition duration-200 hover:bg-blue-950'
+        onClick={(): void => router.push(`/holidays/${holidayDest}`)}
       >
         Search
       </button>
