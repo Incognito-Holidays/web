87fdcbaadb1e023e15e19d1867f2950e1983ffc6
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2023-09-04 07:25:31 +0530

    refactor: hotels page

--- src/app/hotels/page.tsx
+++ src/app/hotels/page.tsx
@@ -1,18 +1,152 @@
-import FormCard from '@components/form-card';
-const Page: React.FC = () => {
+'use client';
+
+import { useState } from 'react';
+import Form from '@components/form';
+
+const HotelsPage: React.FC = () => {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [place, setPlace] = useState('');
+  const [checkIn, setCheckIn] = useState('');
+  const [checkOut, setCheckOut] = useState('');
+  const [rooms, setRooms] = useState('');
+  const [adults, setAdults] = useState('');
+  const [children, setChildren] = useState('');
+  const [message, setMessage] = useState('');
+
   return (
-    <div>
-      <FormCard
-        title='Contact For Hotel Booking'
-        description='Contact us for booking hotel'
-        name='Name'
-        phone_number='Phone Number'
-        email_id='Email Id'
-        subject='Region, City, Area'
-        your_message='Your Message'
-      />
-    </div>
+    <Form
+      title='Contact For Hotel Booking'
+      description='Contact us for booking hotel'
+      name={name}
+      setName={setName}
+      phone={phone}
+      setPhone={setPhone}
+      email={email}
+      setEmail={setEmail}
+    >
+      <div className='flex flex-col gap-8 sm:flex-row sm:items-center'>
+        <div className='grow'>
+          <label
+            htmlFor='place'
+            className='mb-2 block text-sm font-medium text-gray-900'
+          >
+            Region, City, Area (Worldwide)
+          </label>
+          <input
+            id='place'
+            type='text'
+            value={place}
+            onChange={(e): void => setPlace(e.target.value)}
+            placeholder='Select destination'
+            required
+            className='focus:ring-primary-500 focus:border-primary-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 shadow-sm'
+          />
+        </div>
+        <div>
+          <label
+            htmlFor='checkIn'
+            className='mb-2 block text-sm font-medium text-gray-900'
+          >
+            Check In
+          </label>
+          <input
+            id='checkIn'
+            type='date'
+            value={checkIn}
+            onChange={(e): void => setCheckIn(e.target.value)}
+            required
+            className='focus:ring-primary-500 focus:border-primary-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 shadow-sm'
+          />
+        </div>
+        <div>
+          <label
+            htmlFor='checkOut'
+            className='mb-2 block text-sm font-medium text-gray-900'
+          >
+            Check Out
+          </label>
+          <input
+            id='checkOut'
+            type='date'
+            value={checkOut}
+            onChange={(e): void => setCheckOut(e.target.value)}
+            required
+            className='focus:ring-primary-500 focus:border-primary-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 shadow-sm'
+          />
+        </div>
+      </div>
+      <div className='flex flex-col gap-8 min-[450px]:flex-row min-[450px]:items-center'>
+        <div>
+          <label
+            htmlFor='rooms'
+            className='mb-2 block text-sm font-medium text-gray-900'
+          >
+            Number of rooms
+          </label>
+          <input
+            id='rooms'
+            type='number'
+            value={rooms}
+            onChange={(e): void => setRooms(e.target.value)}
+            placeholder='1'
+            required
+            className='focus:ring-primary-500 focus:border-primary-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 shadow-sm'
+          />
+        </div>
+        <div className='grow'>
+          <label
+            htmlFor='adults'
+            className='mb-2 block text-sm font-medium text-gray-900'
+          >
+            Adults
+          </label>
+          <input
+            id='adults'
+            type='number'
+            value={adults}
+            onChange={(e): void => setAdults(e.target.value)}
+            placeholder='1'
+            required
+            className='focus:ring-primary-500 focus:border-primary-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 shadow-sm'
+          />
+        </div>
+        <div>
+          <label
+            htmlFor='children'
+            className='mb-2 block text-sm font-medium text-gray-900'
+          >
+            Children
+          </label>
+          <input
+            id='children'
+            type='number'
+            value={children}
+            onChange={(e): void => setChildren(e.target.value)}
+            placeholder='0'
+            className='focus:ring-primary-500 focus:border-primary-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 shadow-sm'
+          />
+        </div>
+      </div>
+      <div className='sm:col-span-2'>
+        <label
+          htmlFor='message'
+          className='mb-2 block text-sm font-medium text-gray-900'
+        >
+          Want to share something more? Let us know your Enquiry
+        </label>
+        <textarea
+          id='message'
+          rows={6}
+          value={message}
+          onChange={(e): void => setMessage(e.target.value)}
+          placeholder='Your message'
+          className='focus:ring-primary-500 focus:border-primary-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm'
+        />
+      </div>
+    </Form>
   );
 };
 
-export default Page;
+export default HotelsPage;
