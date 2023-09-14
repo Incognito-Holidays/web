5755a849a914ad905c6af7e5a4d209a37af66ad3
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2023-09-15 03:20:25 +0530

    refactor(fix): add missing mobile navbar links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,10 +4,21 @@ import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { HiBars3, HiXMark } from 'react-icons/hi2';
+
 import logo from '@public/assets/incognito-logo.png';
 
 const Navbar: React.FC = () => {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  const navLinks = [
+    { label: 'Home', href: '/' },
+    { label: 'Holidays', href: '/holidays' },
+    { label: 'Hotels', href: '/hotels' },
+    { label: 'Flights', href: '/flights' },
+    { label: 'Trains', href: '/trains' },
+    { label: 'About', href: '/about' },
+    { label: 'Contact', href: '/contact' }
+  ];
 
   return (
     <nav className='border-b-2'>
@@ -18,48 +29,15 @@ const Navbar: React.FC = () => {
           </Link>
 
           <div className='hidden items-center space-x-12 md:flex'>
-            <Link
-              href='/'
-              className='border-b-4 border-transparent pb-1 font-medium text-blue-950 transition duration-200 hover:border-cyan-500 hover:text-cyan-500'
-            >
-              Home
-            </Link>
-            <Link
-              href='/holidays'
-              className='border-b-4 border-transparent pb-1 font-medium text-blue-950 transition duration-200 hover:border-cyan-500 hover:text-cyan-500'
-            >
-              Holidays
-            </Link>
-            <Link
-              href='/hotels'
-              className='border-b-4 border-transparent pb-1 font-medium text-blue-950 transition duration-200 hover:border-cyan-500 hover:text-cyan-500'
-            >
-              Hotels
-            </Link>
-            <Link
-              href='/flights'
-              className='border-b-4 border-transparent pb-1 font-medium text-blue-950 transition duration-200 hover:border-cyan-500 hover:text-cyan-500'
-            >
-              Flights
-            </Link>
-            <Link
-              href='/trains'
-              className='border-b-4 border-transparent pb-1 font-medium text-blue-950 transition duration-200 hover:border-cyan-500 hover:text-cyan-500'
-            >
-              Trains
-            </Link>
-            <Link
-              href='/about'
-              className='border-b-4 border-transparent pb-1 font-medium text-blue-950 transition duration-200 hover:border-cyan-500 hover:text-cyan-500'
-            >
-              About
-            </Link>
-            <Link
-              href='/contact'
-              className='border-b-4 border-transparent pb-1 font-medium text-blue-950 transition duration-200 hover:border-cyan-500 hover:text-cyan-500'
-            >
-              Contact
-            </Link>
+            {navLinks.map((link) => (
+              <Link
+                key={link.href}
+                href={link.href}
+                className='border-b-4 border-transparent pb-1 font-medium text-blue-950 transition duration-200 hover:border-cyan-500 hover:text-cyan-500'
+              >
+                {link.label}
+              </Link>
+            ))}
           </div>
 
           <div className='block md:hidden'>
@@ -79,23 +57,15 @@ const Navbar: React.FC = () => {
           showMobileMenu ? 'block' : 'hidden'
         } space-y-2 bg-gray-100 px-4 py-2`}
       >
-        <Link href='/' className='block text-sm font-medium'>
-          Home
-        </Link>
-        <Link href='/holidays' className='block text-sm font-medium'>
-          Holidays
-        </Link>
-        <Link href='/hotels' className='block text-sm font-medium'>
-          Hotels
-        </Link>
-        <button className='block text-sm font-medium'>Flights</button>
-        <Link href='/trains' className='block text-sm font-medium'>
-          Trains
-        </Link>
-        <button className='block text-sm font-medium'>About</button>
-        <Link href='/contact' className='block text-sm font-medium'>
-          Contact
-        </Link>
+        {navLinks.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className='block text-sm font-medium'
+          >
+            {link.label}
+          </Link>
+        ))}
       </div>
     </nav>
   );
