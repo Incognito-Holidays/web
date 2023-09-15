7dcf7a479d6619fe873cc84d83f7dc453b479fd9
Debadyuti <debadyuti.karmakar@gmail.com>
2023-09-15 09:43:39 +0530

    Delete unnecessary code

--- src/components/travel-blogs.tsx
+++ src/components/travel-blogs.tsx
@@ -94,12 +94,6 @@ const TravelBlogs: React.FC = () => {
                   <p className='mb-3 text-sm font-normal text-gray-900'>
                     {slide.desc.substring(0, 205)}...
                   </p>
-                  {/* <a href='#' className='inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
-            Read more
-             <svg className='w-3.5 h-3.5 ml-2' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 14 10'>
-                <path stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M1 5h12m0 0L9 1m4 4L9 9'/>
-            </svg>
-        </a> */}
                 </div>
               </div>
             </SwiperSlide>
