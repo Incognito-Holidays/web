8d545ce5529e67ea7be85bf75008e537268b997e
lucy2512 <mail4parthib@gmail.com>
2023-10-09 22:02:18 +0530

    feat: add modal

---
+++ src/components/booknow-button.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useState } from 'react';
+import { Button } from './ui/button';
+import Modal from './modal';
+import type { Package } from '@lib/types';
+
+const BookNowButton = ({ title }: Pick<Package, 'title'>) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <>
+      <Button
+        onClick={(): void => setIsModalOpen(true)}
+        className='w-full rounded-full'
+      >
+        BOOK NOW
+      </Button>
+
+      <Modal
+        isOpen={isModalOpen}
+        setIsOpen={setIsModalOpen}
+        selectedPackage={title}
+      />
+    </>
+  );
+};
+
+export default BookNowButton;
