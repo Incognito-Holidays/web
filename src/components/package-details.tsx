31ec0ac8a74935dfe8969b3eb3bb59579e10e5da
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2023-10-09 22:43:52 +0530

    chore(deps): remove unused deps & minor change

--- src/components/package-details.tsx
+++ src/components/package-details.tsx
@@ -3,6 +3,7 @@
 import { Tab } from '@headlessui/react';
 import { PortableText } from '@portabletext/react';
 import { FaStar } from 'react-icons/fa';
+import { cn } from '@lib/utils';
 import type { PortableTextBlock } from 'sanity';
 
 type Props = {
@@ -21,16 +22,13 @@ const PackageDetails = ({
   termsConditions,
   hotel
 }: Props) => {
-  function classNames(...classes: string[]): string {
-    return classes.filter(Boolean).join(' ');
-  }
   return (
     <div>
       <Tab.Group>
         <Tab.List className='flex space-x-1 rounded-xl bg-blue-950 p-1'>
           <Tab
             className={({ selected }): string =>
-              classNames(
+              cn(
                 'w-full rounded-lg py-1 text-sm font-medium leading-5 sm:py-2.5',
                 'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
                 selected
@@ -43,7 +41,7 @@ const PackageDetails = ({
           </Tab>
           <Tab
             className={({ selected }): string =>
-              classNames(
+              cn(
                 'w-full rounded-lg py-1 text-sm font-medium leading-5 sm:py-2.5',
                 'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
                 selected
@@ -56,7 +54,7 @@ const PackageDetails = ({
           </Tab>
           <Tab
             className={({ selected }): string =>
-              classNames(
+              cn(
                 'w-full rounded-lg py-1 text-sm font-medium leading-5 sm:py-2.5',
                 'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
                 selected
@@ -70,7 +68,7 @@ const PackageDetails = ({
         </Tab.List>
         <Tab.Panels className='!mt-4 rounded-2xl border-2 border-gray-300'>
           <Tab.Panel
-            className={classNames(
+            className={cn(
               'prose space-y-4 rounded-xl bg-white p-3',
               'focus:outline-none'
             )}
@@ -78,15 +76,15 @@ const PackageDetails = ({
             <PortableText value={itinerary} />
           </Tab.Panel>
           <Tab.Panel
-            className={classNames(
-              ' prose space-y-4 rounded-xl bg-white p-3',
+            className={cn(
+              'prose space-y-4 rounded-xl bg-white p-3',
               'focus:outline-none'
             )}
           >
             <PortableText value={inclusionExclusion} />
           </Tab.Panel>
           <Tab.Panel
-            className={classNames(
+            className={cn(
               'prose space-y-4 rounded-xl bg-white p-3',
               'focus:outline-none'
             )}
