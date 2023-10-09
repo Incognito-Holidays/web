b2e2c1ac23d2de5a7a4c0dc1c1f5ad691388700f
Debadyuti <debadyuti.karmakar@gmail.com>
2023-10-09 17:57:50 +0530

    add blog page design

--- src/app/(site)/blog/[slug]/page.tsx
+++ src/app/(site)/blog/[slug]/page.tsx
@@ -13,14 +13,15 @@ const BlogPage = async ({ params }: BlogPageProps) => {
   const data = await getBlog(slug);
 
   return (
-    <main>
+    <main className='prose prose-base max-w-none p-10 prose-h1:text-violet-950 prose-img:rounded-xl'>
       <h1>{data.title}</h1>
       <Image
         src={data.coverImage.asset.url}
         alt={data.coverImage.alt}
-        width={1000}
-        height={1000}
+        width={1200}
+        height={500}
         placeholder='blur'
+        className=' h-[30rem] w-full  object-cover object-center'
         blurDataURL={data.coverImage.asset.lqip}
       />
       <h3>{data.subtitle}</h3>
