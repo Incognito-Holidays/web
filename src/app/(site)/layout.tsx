af8e2c08d477bba48016fdd7dc82f23e35b101e4
Debadyuti <debadyuti.karmakar@gmail.com>
2023-11-18 20:42:07 +0530

    meta link & file for DM

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-quotes */
 import '@styles/global.css';
 import Script from 'next/script';
 import { Inter } from 'next/font/google';
@@ -11,7 +12,8 @@ import type { PropsWithChildren } from 'react';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Incognito Holidays',
+  title:
+    'Best International Tours & Travel Agency in Kolkata & Chennai - Incognito Holidays',
   description: 'Your travel companion'
 };
 
@@ -28,6 +30,31 @@ const SiteRootLayout = ({ children }: PropsWithChildren) => {
           gtag('config', 'G-Z6DY8YHM9W');
         `}
       </Script>
+      <meta
+        property='og:title'
+        content='Travel Agents Kolkata: Best Tour and Travel Agency in Kolkata | Incognito'
+      />
+      <meta property='og:site_name' content='Incognito Holidays' />{' '}
+      <meta property='og:url' content='https://www.incognitoholidays.com' />{' '}
+      <meta
+        property='og:description'
+        content='Browse the list of the best travel agency in Kolkata. Book your tour with Incognito Holidays top travel agency in Kolkata,Chennai for domestic and International trip.'
+      />
+      <meta property='og:type' content='Tour and Travel website' />{' '}
+      <meta
+        property='og:image'
+        content='https://www.incognitoholidays.com/_next/image?url=%2Fassets%2Fpop-up.jpg&w=3840&q=75'
+      />
+      <link rel='canonical' href='https://www.incognitoholidays.com' />
+      <meta
+        name='description'
+        content='Best Travel agency at Kolkata,Chennai offers famous travel agents for customized &amp; group tour packages, call our tour operators for International tour, Hotels, Flight etc.&nbsp;
+'
+      />
+      <meta
+        name='keyword'
+        content='Incognito holidays is the best travel agency & package tour operator in  Kolkata.Thailand tour operator, Srilanka Tour,bali, indonesia packages,bali holiday packages including flights,thailand holiday packages,thailand trip cost from india'
+      />
       <body className={inter.className}>
         <Header />
         {children}
